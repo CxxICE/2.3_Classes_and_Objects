d7e572c90c3218f7af10ba847bcb96ff5cc92303
Ilia Surikov <surikovis89@gmail.com>
2023-01-28 20:19:17 +0300

    Initial commit

---
+++ 2.3.1_Calculator/2.3.1_Calculator/2.3.1_Calculator.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+
+class Calculator
+{
+public:
+	double add();
+	double multiply();
+	double subtract_1_2();
+	double subtract_2_1();
+	double divide_1_2();
+	double divide_2_1();
+	bool set_num1(double num1);
+	bool set_num2(double num2);
+private:
+	double num1;
+	double num2;
+};
+
+double Calculator::add()
+{
+	return num1 + num2;
+}
+
+double Calculator::multiply()
+{
+	return num1 * num2;
+}
+
+double Calculator::subtract_1_2()
+{
+	return num1 - num2;
+}
+
+double Calculator::subtract_2_1()
+{
+	return num2 - num1;
+}
+
+double Calculator::divide_1_2()
+{
+	return num1 / num2;
+}
+
+double Calculator::divide_2_1()
+{
+	return num2 / num1;
+}
+
+bool Calculator::set_num1(double num1)
+{
+	if (num1 == 0)
+	{
+		return false;
+	}
+	else
+	{
+		this->num1 = num1;
+		return true;
+	}
+}
+
+bool Calculator::set_num2(double num2)
+{
+	if (num2 == 0)
+	{
+		return false;
+	}
+	else
+	{
+		this->num2 = num2;
+		return true;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	double num1, num2;
+	Calculator calc;
+
+	do
+	{
+		do
+		{
+			std::cout << "Введите num1: ";
+			std::cin >> num1;
+			if (!calc.set_num1(num1))
+			{
+				std::cout << "Неверный ввод!" << '\n';
+			}
+			else 
+			{
+				break;
+			}
+		} while (true);
+		
+		do
+		{
+			std::cout << "Введите num2: ";
+			std::cin >> num2;
+			if (!calc.set_num2(num2))
+			{
+				std::cout << "Неверный ввод!" << '\n';
+			}
+			else 
+			{
+				break;
+			}
+		} while (true);
+				
+		std::cout << "num1 + num2 = " << calc.add() << '\n';
+		std::cout << "num1 - num2 = " << calc.subtract_1_2() << '\n';
+		std::cout << "num2 - num1 = " << calc.subtract_2_1() << '\n';
+		std::cout << "num1 * num2 = " << calc.multiply() << '\n';
+		std::cout << "num1 / num2 = " << calc.divide_1_2() << '\n';
+		std::cout << "num2 / num1 = " << calc.divide_2_1() << '\n';
+
+	} while (true);  
+	std::system("pause");
+}
+
+
---
+++ 2.3.2_Counter/2.3.2_Counter/2.3.2_Counter.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+class Counter
+{
+public:
+	Counter();
+	Counter(int count);
+	void plus();
+	void minus();
+	int equals();
+private:
+	int count;
+};
+
+Counter::Counter()
+{
+	this->count = 1;
+}
+
+Counter::Counter(int count)
+{
+	this->count = count;
+}
+
+void Counter::plus()
+{
+	++count;
+}
+
+void Counter::minus()
+{
+	--count;
+}
+
+int Counter::equals()
+{
+	return count;
+}
+
+void counting(Counter &count)
+{
+	std::string inText;
+	do
+	{
+		std::cout << "Введите команду ('+', '-', '=' или 'x'):";
+		std::cin >> inText;
+		if (inText == "+")
+		{
+			count.plus();
+		}
+		else if (inText == "-")
+		{
+			count.minus();
+		}
+		else if (inText == "=")
+		{
+			std::cout << count.equals() << '\n';
+		}
+		else if (inText == "x")
+		{
+			std::cout << "До свидания!\n";
+			break;
+		}
+		else
+		{
+			std::cout << "Неверный ввод!\n";
+		}
+	} while (true);    
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	std::string inText;
+	int start;
+	do
+	{
+		std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет:";
+		std::cin >> inText;
+		if (inText == "да")
+		{
+			std::cout << "Введите начальное значение счётчика:";
+			std::cin >> start;
+			Counter count(start);
+			counting(count);
+			break;
+		}
+		else if (inText == "нет")
+		{
+			Counter count;
+			counting(count);
+			break;
+		}		
+		else
+		{
+			std::cout << "Неверный ввод!\n";
+		}
+	} while (true);	
+	std::system("pause");
+}
